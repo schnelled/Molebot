f7eb57f22962213aeffa95b952633b9cc160f901
schnelled <shnelle@pdx.edu>
2018-06-19 15:57:15 -0700

    Code to move both wheels

--- MotorControl/simpleMovement.py
+++ MotorControl/simpleMovement.py
@@ -4,8 +4,10 @@ import time
 import RPi.GPIO as GPIO
 
 # Define the constant variables
-FORWARD = 26
-BACKWARD = 20
+FORWARD_RIGHT = 26
+BACKWARD_RIGHT = 20
+FORWARD_LEFT = 19
+FORWARD_LEFT = 16
 
 # Cleanup the GPIO before starting
 GPIO.cleanup()
@@ -14,16 +16,19 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
 # Setup the GPIO input and output
-GPIO.setup(FORWARD, GPIO.OUT)
-GPIO.setup(BACKWARD, GPIO.OUT)
+GPIO.setup(FORWARD_RIGHT, GPIO.OUT)
+GPIO.setup(BACKWARD_RIGHT, GPIO.OUT)
+GPIO.setup(FORWARD_LEFT, GPIO.OUT)
+GPIO.setup(BACKWARD_LEFT, GPIO.OUT)
 
 # Function: 	forward
-# Input: 	x - the amount of sleep
-# Output: 	void
+# Input: 		x - the amount of sleep
+# Output: 		void
 # Definition:	Move the wheel in the forward direction
 def forward(x):
 	# Set the GPIO forward movement output to HIGH
-	GPIO.output(FORWARD, GPIO.HIGH)
+	GPIO.output(FORWARD_RIGHT, GPIO.HIGH)
+	GPIO.output(FORWARD_LEFT, GPIO.HIGH)
 
 	# Display message
 	print("Moving Forward")
@@ -32,7 +37,8 @@ def forward(x):
 	time.sleep(x)
 
 	# Set the GPIO forward movement output to LOW
-	GPIO.output(FORWARD, GPIO.LOW)
+	GPIO.output(FORWARD_RIGHT, GPIO.LOW)
+	GPIO.output(FORWARD_LEFT, GPIO.LOW)
 
 # Function:	reverse
 # Input:	x - the amount of sleep
@@ -40,7 +46,8 @@ def forward(x):
 # Definition:	Move the wheel in the reverse direction
 def reverse(x):
 	# Set the GPIO reverse movement output to HIGH
-	GPIO.output(BACKWARD, GPIO.HIGH)
+	GPIO.output(BACKWARD_RIGHT, GPIO.HIGH)
+	GPIO.output(BACKWARD_LEFT, GPIO.HIGH)
 
 	# Display message
 	print("Moving Backward")
@@ -49,7 +56,8 @@ def reverse(x):
 	time.sleep(x)
 
 	# Set the GPIO reverse movement output to LOW
-	GPIO.output(BACKWARD, GPIO.LOW)
+	GPIO.output(BACKWARD_RIGHT, GPIO.LOW)
+	GPIO.output(BACKWARD_LEFT, GPIO.LOW)
 
 # Continuously loop
 while(True):
