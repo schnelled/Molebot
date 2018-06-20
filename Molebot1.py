bf918cdb93436e96546856f6e33d786dc9fafa7e
schnelled <shnelle@pdx.edu>
2018-06-19 20:08:22 -0700

    Initial movement fully working

--- Molebot1.py
+++ Molebot1.py
@@ -16,25 +16,39 @@ GPIO.setup(MotorControl.BACKWARD_RIGHT, GPIO.OUT)
 GPIO.setup(MotorControl.FORWARD_LEFT, GPIO.OUT)
 GPIO.setup(MotorControl.BACKWARD_LEFT, GPIO.OUT)
 
-# Continuously loop
-while(True):
-    # Move both wheels forward
-    MotorControl.forward(5)
+try:
+    # Continuously loop
+    while(True):
+        # Move both wheels forward
+        MotorControl.forward(5)
 
-    # Move both wheels backward
-    MotorControl.reverse(5)
+        # Move both wheels backward
+        MotorControl.reverse(5)
 
-    # Turn to the right
-    #MotorControl.turn_right(5)
+        # Turn to the right
+        MotorControl.turn_right(5)
 
-    # Turn to the left
-    #MotorControl.turn_left(5)
+        # Turn to the left
+        MotorControl.turn_left(5)
 
-    # Spin to the right
-    #MotorControl.spin_right(5)
+        # Spin to the right
+        MotorControl.spin_right(5)
 
-    # Spin to the left
-    #MotorControl.spin_left(5)
+        # Spin to the left
+        MotorControl.spin_left(5)
 
-# Cleanup the GPIO
-GPIO.cleanup()
+# Interrupt from keyboard
+except KeyboardInterrupt:
+    # Display interrupt message
+    print("Exiting Program")
+
+# Error occured
+except:
+    # Display error message
+    print("Error occurs, exiting program")
+
+# Cleanup the GPIO and exit the program
+finally:
+    # Cleanup the GPIO
+    GPIO.cleanup()
+    sys.exit()
