32cf391aa92b015e0929412fac6a3e792aa2e0b5
schnelled <schnelle@pdx.edu>
2018-06-20 13:18:34 -0700

    Fixed indetation errors produced by Atom

--- Molebot2.py
+++ Molebot2.py
@@ -3,15 +3,12 @@ from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
 import time
 import atexit
 
-# Create a default stepper motor HAT object
-mh = Raspi_MotorHAT(addr=0x6f)
-
 # Function:     turnOffMotors
 # Input:        none
 # Output:       void
 # Description:  Auto-disables motors on shutdown
-def turnOffMotors():
-    mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
+def turnOffMotors(mh):
+    	mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
 	mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
 	mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
 	mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
@@ -29,37 +26,37 @@ rightMotor = mh.getMotor(3)
 while (True):
 	print "Forward! "
 	leftMotor.run(Raspi_MotorHAT.FORWARD)
-    rightMotor.run(Raspi_MotorHAT.FORWARD)
+    	rightMotor.run(Raspi_MotorHAT.FORWARD)
 
 	print "\tSpeed up..."
 	for i in range(255):
 		leftMotor.setSpeed(i)
-        rightMotor.setSpeed(i)
+        	rightMotor.setSpeed(i)
 		time.sleep(0.01)
 
 	print "\tSlow down..."
 	for i in reversed(range(255)):
 		leftMotor.setSpeed(i)
-        rightMotor.setSpeed(i)
+        	rightMotor.setSpeed(i)
 		time.sleep(0.01)
 
 	print "Backward! "
 	leftMotor.run(Raspi_MotorHAT.BACKWARD)
-    rightMotor.run(Raspi_MotorHAT.BACKWARD)
+    	rightMotor.run(Raspi_MotorHAT.BACKWARD)
 
 	print "\tSpeed up..."
 	for i in range(255):
 		leftMotor.setSpeed(i)
-        rightMotor.setSpeed(i)
+        	rightMotor.setSpeed(i)
 		time.sleep(0.01)
 
 	print "\tSlow down..."
 	for i in reversed(range(255)):
 		leftMotor.setSpeed(i)
-        rightMotor.setSpeed(i)
+        	rightMotor.setSpeed(i)
 		time.sleep(0.01)
 
 	print "Release"
 	leftMotor.run(Raspi_MotorHAT.RELEASE)
-    rightMotor.run(Raspi_MotorHAT.RELEASE)
+    	rightMotor.run(Raspi_MotorHAT.RELEASE)
 	time.sleep(1.0)
