9233c4abbcebd7ba43fb8bc289bc8a2243a5f819
schnelled <shnelle@pdx.edu>
2018-06-22 12:07:06 -0700

    Added keyboard control

--- MotorControl.py
+++ MotorControl.py
@@ -13,7 +13,7 @@ BACKWARD_LEFT = 16
 # Output: 		void
 # Definition:	Move forward both wheels
 def forward(x):
-	# Set both the GPIO's forward output to HIGH
+	# Set both the forward output to HIGH
 	GPIO.output(FORWARD_RIGHT, GPIO.HIGH)
 	GPIO.output(FORWARD_LEFT, GPIO.HIGH)
 
@@ -23,7 +23,7 @@ def forward(x):
 	# Sleep for x time
 	time.sleep(x)
 
-	# Set both the GPIO's forward movement output to LOW
+	# Set both the forward movement output to LOW
 	GPIO.output(FORWARD_RIGHT, GPIO.LOW)
 	GPIO.output(FORWARD_LEFT, GPIO.LOW)
 
@@ -32,92 +32,110 @@ def forward(x):
 # Output:		void
 # Definition:	Move in reverse both wheels
 def reverse(x):
-    # Set both the GPIO's reverse output to HIGH
-    GPIO.output(BACKWARD_RIGHT, GPIO.HIGH)
-    GPIO.output(BACKWARD_LEFT, GPIO.HIGH)
+    	# Set both the reverse output to HIGH
+    	GPIO.output(BACKWARD_RIGHT, GPIO.HIGH)
+    	GPIO.output(BACKWARD_LEFT, GPIO.HIGH)
 
-    # Display message
-    print("Moving Backward")
+   	# Display message
+   	print("Moving Backward")
 
-    # Sleep for x time
-    time.sleep(x)
+    	# Sleep for x time
+    	time.sleep(x)
 
-    # Set both the GPIO's reverse output to LOW
-    GPIO.output(BACKWARD_RIGHT, GPIO.LOW)
-    GPIO.output(BACKWARD_LEFT, GPIO.LOW)
+    	# Set both the reverse output to LOW
+    	GPIO.output(BACKWARD_RIGHT, GPIO.LOW)
+    	GPIO.output(BACKWARD_LEFT, GPIO.LOW)
 
 # Function:     turn_right
 # Input:        x - the amount of sleep
 # Output:       void
 # Definition:   Move the right wheel forward
 def turn_right(x):
-    # Set the GPIO's right forward output to HIGH
-    GPIO.output(FORWARD_RIGHT, GPIO.HIGH)
+    	# Set the right forward output to HIGH
+    	GPIO.output(FORWARD_RIGHT, GPIO.HIGH)
 
-    # Display message
-    print("Moving Right")
+    	# Display message
+    	print("Moving Right")
 
-    # Sleep for x time
-    time.sleep(x)
+    	# Sleep for x time
+    	time.sleep(x)
 
-    # Set the GPIO's right forward output to LOW
-    GPIO.output(FORWARD_RIGHT, GPIO.LOW)
+    	# Set the right forward output to LOW
+    	GPIO.output(FORWARD_RIGHT, GPIO.LOW)
 
 # Function:     turn_left
 # Input:        x - the amount of sleep
 # Output:       void
 # Definition:   Move the left wheel forward
 def turn_left(x):
-    # Set the GPIO's left forward output to HIGH
-    GPIO.output(FORWARD_LEFT, GPIO.HIGH)
+    	# Set the left forward output to HIGH
+    	GPIO.output(FORWARD_LEFT, GPIO.HIGH)
 
-    # Display message
-    print("Moving Left")
+    	# Display message
+    	print("Moving Left")
 
-    # Sleep for x time
-    time.sleep(x)
+    	# Sleep for x time
+    	time.sleep(x)
 
-    # Set the GPIO's left forward output to LOW
-    GPIO.output(FORWARD_LEFT, GPIO.LOW)
+    	# Set the left forward output to LOW
+   	GPIO.output(FORWARD_LEFT, GPIO.LOW)
 
 # Function:     double_right
 # Input:        x - the amount of sleep
 # Output:       void
 # Definition:   Turn the right wheel forward and the left wheel backward
 def spin_right(x):
-    # Set the GPIO's right forward output to HIGH
-    # Set the GPIO's left reverse output to HIGH
-    GPIO.output(FORWARD_RIGHT, GPIO.HIGH)
-    GPIO.output(BACKWARD_LEFT, GPIO.HIGH)
+    	# Set the right forward output to HIGH
+    	# Set the left reverse output to HIGH
+    	GPIO.output(FORWARD_RIGHT, GPIO.HIGH)
+    	GPIO.output(BACKWARD_LEFT, GPIO.HIGH)
 
-    # Display message
-    print("Spinning Right")
+    	# Display message
+    	print("Spinning Right")
 
-    # Sleep for x time
-    time.sleep(x)
+    	# Sleep for x time
+    	time.sleep(x)
 
-    # Set the GPIO's right forward output to LOW
-    # Set the GPIO's left reverse output to LOW
-    GPIO.output(FORWARD_RIGHT, GPIO.LOW)
-    GPIO.output(BACKWARD_LEFT, GPIO.LOW)
+    	# Set the right forward output to LOW
+    	# Set the left reverse output to LOW
+    	GPIO.output(FORWARD_RIGHT, GPIO.LOW)
+    	GPIO.output(BACKWARD_LEFT, GPIO.LOW)
 
 # Function:     double_left
 # Input:        x - the amount of sleep
 # Output:       void
 # Definition:   Turn the left wheel forward and the right wheel backward
 def spin_left(x):
-    # Move the GPIO's left forward output to HIGH
-    # Move the GPIO's right reverse output to HIGH
-    GPIO.output(FORWARD_LEFT, GPIO.HIGH)
-    GPIO.output(BACKWARD_RIGHT, GPIO.HIGH)
+    	# Move the left forward output to HIGH
+    	# Move the right reverse output to HIGH
+    	GPIO.output(FORWARD_LEFT, GPIO.HIGH)
+    	GPIO.output(BACKWARD_RIGHT, GPIO.HIGH)
 
-    # Display message
-    print("Spinning Left")
+    	# Display message
+    	print("Spinning Left")
 
-    # Sleep for x time
-    time.sleep(x)
+    	# Sleep for x time
+    	time.sleep(x)
 
-    # Set the GPIO's left forward output to LOW
-    # Set the GPIO's right forward output to LOW
-    GPIO.output(FORWARD_LEFT, GPIO.LOW)
-    GPIO.output(BACKWARD_RIGHT, GPIO.LOW)
+    	# Set the left forward output to LOW
+    	# Set the right forward output to LOW
+    	GPIO.output(FORWARD_LEFT, GPIO.LOW)
+    	GPIO.output(BACKWARD_RIGHT, GPIO.LOW)
+
+# Function:		stop
+# Input:		x -  the amount of sleep
+# Output:		void
+# Definition:	Both wheels stop movement
+def stop(x):
+	# Set the all GPIOs to low
+	GPIO.output(FORWARD_LEFT, GPIO.LOW)
+	GPIO.output(FORWARD_RIGHT, GPIO.LOW)
+	GPIO.output(BACKWARD_LEFT, GPIO.LOW)
+	GPIO.output(BACKWARD_RIGHT, GPIO.LOW)
+
+	# Display message
+    	print("Stopping")
+
+    	# Sleep for x time
+    	time.sleep(x)
+	
