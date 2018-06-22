4c48672973147cb21fecee97ce11acbaa3b6cee0
schnelled <shnelle@pdx.edu>
2018-06-22 12:15:40 -0700

    Added keyboard control

--- Molebot3.py
+++ Molebot3.py
@@ -14,46 +14,37 @@ atexit.register(MotorControl.stop(0))
 leftMotor = mh.getMotor(1)
 rightMotor = mh.getMotor(3)
 
-try:
-	# Continuously loop
-	while(True):
+# Continuously loop
+while(True):
 
-		# Command input
-		cmd = input("")
+	# Command input
+	cmd = input("")
 
-		# Forward command
-		if cmd == 'w': MotorControl.forward(1)
+	# Forward command
+	if cmd == 'w': MotorControl.forward(1)
 
-		# Backward command
-		if cmd == 's': MotorControl.reverse(1)
+	# Backward command
+	if cmd == 's': MotorControl.reverse(1)
 
-		# Right turn
-		if cmd == 'd': MotorControl.turn_right(1)
+	# Right turn
+	if cmd == 'd': MotorControl.turn_right(1)
 
-		# Left turn
-		if cmd == 'a': MotorControl.turn_left(1)
+	# Left turn
+	if cmd == 'a': MotorControl.turn_left(1)
 
-		# Right spin
-		if cmd == 'e': MotorControl.spin_right(1)
+	# Right spin
+	if cmd == 'e': MotorControl.spin_right(1)
 
-		# Left spin
-		if cmd == 'q': MotorControl.spin_left(1)
+	# Left spin
+	if cmd == 'q': MotorControl.spin_left(1)
 
-		# Stop
-		if cmd == 'r': MotorControl.stop(1)
-		
-# Interrupt from keyboard
-except:
-	# Display exit interrupt message
-	print("Exiting program")
+	# Stop
+	if cmd == 'r': MotorControl.stop(1)
 
-# Error occured
-except:
-	# Display error message
-	print("Error occurs, exiting program")
-
-# Cleanup the GPIO and exit the program
-finally:
-	GPIO.cleanup()
-	sys.exit()
+	# Kill the program
+	if cmd == 'p':
+		# Display exit interrupt message
+		print("Exiting program")
+		GPIO.cleanup()
+		sys.exit()
 
