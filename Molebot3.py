752867b05a4a8d727d16077b14be8eb684320fff
schnelled <shnelle@pdx.edu>
2018-06-22 13:26:26 -0700

    Added keyboard control

--- Molebot3.py
+++ Molebot3.py
@@ -3,6 +3,7 @@ from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
 import MotorControl
 import atexit
 import sys
+import string
 
 ####################################################
 # Function:     turnOffMotors
