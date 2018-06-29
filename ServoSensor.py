2ecf3d757e6eb6a338260217239f3b19cdaa6a5f
schnelled <shnelle@pdx.edu>
2018-06-28 18:44:42 -0700

    Initail testing for position 1

--- ServoSensor.py
+++ ServoSensor.py
@@ -1,3 +1,46 @@
 # Import needed modules
 import ServoControl
 import SensorControl
+
+# Define constants
+NUMBER_TRAILS = 5
+FAR_PAUSE = 2
+NEAR_PAUSE = 1
+
+#####################################################################
+# Function:     step_0
+# Input:        None 
+# Output:       None - void
+# Description:	Move the servo to the far right position and obtains
+#	distance to object at far right position
+#####################################################################
+def step_0():
+	# Move the servo to the far right position
+	ServoControl.step_0()
+
+	# Pause
+	time.sleep(FAR_PAUSE)
+
+	# Obtain the distance
+	distance = SensorControl.distance()
+
+	# Pause
+	time.sleep(FAR_PAUSE)
+
+	# Return the distance
+	return distance
+
+# Define testing functionality
+if __name__ == "__main__":
+	# Initialize a counter
+	counter = 0
+
+	# Loop for a fixed amount of trials
+	while counter < NUMBER_TRIALS:
+
+		# Move to step 0 and obtain the distance
+		tempDist = step_0()
+
+		# Display the distance
+		print "Step0 distance: " + str(tempDist)
+		
